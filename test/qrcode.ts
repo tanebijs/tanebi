045d5d474b90d2c60b452759839ddbbaf0b78719
Seijo Cecilia <willamwei@outlook.com>
2025-02-01 01:11:35 +0800

    [core] add test for wtlogin
    Now QR code login is completed

--- test/qrcode.ts
+++ test/qrcode.ts
@@ -3,92 +3,94 @@ import { randomBytes } from 'crypto';
 import { UrlSignProvider } from '@/core/common/SignProvider';
 import { TransEmp12_QrCodeState } from '@/core/packet/login/wtlogin/TransEmp12';
 
-async function main() {
-    const ctx = new BotContext(
-        {
-            'Os': 'Linux',
-            'VendorOs': 'linux',
-            'Kernel': 'Linux',
-            'CurrentVersion': '3.2.15-30366',
-            'MiscBitmap': 32764,
-            'PtVersion': '2.0.0',
-            'SsoVersion': 19,
-            'PackageName': 'com.tencent.qq',
-            'WtLoginSdk': 'nt.wtlogin.0.0.1',
-            'AppId': 1600001615,
-            'SubAppId': 537258424,
-            'AppIdQrCode': 13697054,
-            'AppClientVersion': 30366,
-            'MainSigMap': 169742560,
-            'SubSigMap': 0,
-            'NTLoginType': 1,
+const ctx = new BotContext(
+    {
+        'Os': 'Linux',
+        'VendorOs': 'linux',
+        'Kernel': 'Linux',
+        'CurrentVersion': '3.2.15-30366',
+        'MiscBitmap': 32764,
+        'PtVersion': '2.0.0',
+        'SsoVersion': 19,
+        'PackageName': 'com.tencent.qq',
+        'WtLoginSdk': 'nt.wtlogin.0.0.1',
+        'AppId': 1600001615,
+        'SubAppId': 537258424,
+        'AppIdQrCode': 13697054,
+        'AppClientVersion': 30366,
+        'MainSigMap': 169742560,
+        'SubSigMap': 0,
+        'NTLoginType': 1,
+    },
+    {},
+    {
+        guid: Buffer.from('030201000504070608090A0B0C0D0E0F', 'hex'),
+        macAddress: Buffer.from('010203040506', 'hex'),
+        deviceName: 'LagrangeTS-0abcde',
+        systemKernel: 'Windows 10.0.19042',
+        kernelVersion: '10.0.19042.0',
+    },
+    {
+        uin: 0,
+        passwordMd5: '',
+        stub: {
+            randomKey: randomBytes(16),
+            tgtgtKey: Buffer.alloc(16),
         },
-        {},
-        {
-            guid: Buffer.from('030201000504070608090A0B0C0D0E0F', 'hex'),
-            macAddress: Buffer.from('010203040506', 'hex'),
-            deviceName: 'LagrangeTS-0abcde',
-            systemKernel: 'Windows 10.0.19042',
-            kernelVersion: '10.0.19042.0',
+        session: {
+            d2Key: Buffer.alloc(16),
+            d2: Buffer.alloc(0),
+            tgt: Buffer.alloc(0),
+            sessionDate: new Date(),
+            sequence: 0,
         },
-        {
-            uin: 0,
-            passwordMd5: '',
-            stub: {
-                randomKey: randomBytes(16),
-                tgtgtKey: Buffer.alloc(16),
-            },
-            session: {
-                d2Key: Buffer.alloc(16),
-                d2: Buffer.alloc(0),
-                tgt: Buffer.alloc(0),
-                sessionDate: new Date(),
-                sequence: 0,
-            },
-            info: {
-                age: 0,
-                gender: 0,
-                name: '',
-            },
+        info: {
+            age: 0,
+            gender: 0,
+            name: '',
         },
-        UrlSignProvider('http://106.54.14.24:8084/api/sign/30366'),
-    );
-    await ctx.networkLogic.connectToMsfServer();
+    },
+    UrlSignProvider('http://106.54.14.24:8084/api/sign/30366'),
+);
+await ctx.networkLogic.connectToMsfServer();
 
-    const qrCodeInfo = await ctx.ops.call('fetchQrCode');
-    console.log(qrCodeInfo);
+const qrCodeInfo = await ctx.ops.call('fetchQrCode');
+console.log(qrCodeInfo);
 
-    ctx.keystore.session.qrString = qrCodeInfo.qrSig;
-    ctx.keystore.session.qrSign = qrCodeInfo.signature;
-    ctx.keystore.session.qrUrl = qrCodeInfo.url;
+ctx.keystore.session.qrString = qrCodeInfo.qrSig;
+ctx.keystore.session.qrSign = qrCodeInfo.signature;
+ctx.keystore.session.qrUrl = qrCodeInfo.url;
 
-    const qrCodeResult = await new Promise<{
-        tempPassword: Buffer,
-        noPicSig: Buffer,
-        tgtgtKey: Buffer,
-    }>((resolve, reject) => {
-        const qrCodeResultLoop = setInterval(async () => {
-            const res = await ctx.ops.call('queryQrCodeResult');
-            if (res.confirmed) {
+const qrCodeResult = await new Promise<{
+    tempPassword: Buffer,
+    noPicSig: Buffer,
+    tgtgtKey: Buffer,
+}>((resolve, reject) => {
+    const qrCodeResultLoop = setInterval(async () => {
+        const res = await ctx.ops.call('queryQrCodeResult');
+        if (res.confirmed) {
+            clearInterval(qrCodeResultLoop);
+            resolve({
+                tempPassword: res.tempPassword,
+                noPicSig: res.noPicSig,
+                tgtgtKey: res.tgtgtKey,
+            });
+        } else {
+            if (res.state === TransEmp12_QrCodeState.CodeExpired || res.state === TransEmp12_QrCodeState.Canceled) {
                 clearInterval(qrCodeResultLoop);
-                resolve({
-                    tempPassword: res.tempPassword,
-                    noPicSig: res.noPicSig,
-                    tgtgtKey: res.tgtgtKey,
-                });
-            } else {
-                if (res.state === TransEmp12_QrCodeState.CodeExpired || res.state === TransEmp12_QrCodeState.Canceled) {
-                    clearInterval(qrCodeResultLoop);
-                    reject(new Error('Session expired or cancelled'));
-                }
+                reject(new Error('Session expired or cancelled'));
             }
-        }, 2000);
-    });
-    console.log(qrCodeResult);
+        }
+    }, 2000);
+});
+console.log(qrCodeResult);
 
-    ctx.keystore.session.tempPassword = qrCodeResult.tempPassword;
-    ctx.keystore.session.noPicSig = qrCodeResult.noPicSig;
-    ctx.keystore.stub.tgtgtKey = qrCodeResult.tgtgtKey;
-}
+ctx.keystore.session.tempPassword = qrCodeResult.tempPassword;
+ctx.keystore.session.noPicSig = qrCodeResult.noPicSig;
+ctx.keystore.stub.tgtgtKey = qrCodeResult.tgtgtKey;
 
-main();
+ctx.keystore.uin = await ctx.wtLoginLogic.getCorrectUin();
+console.log(ctx.keystore.uin, 'trying to login');
+
+const loginResult = await ctx.ops.call('wtLogin');
+console.log(loginResult);
