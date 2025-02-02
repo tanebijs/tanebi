94729d9ae7ed329a959fe761b9b9d8cc47e6ff34
Seijo Cecilia <willamwei@outlook.com>
2025-02-02 11:25:56 +0800

    [core] add difinition of Attribute

--- src/core/packet/message/RichText.ts
+++ src/core/packet/message/RichText.ts
@@ -1,8 +1,9 @@
 import { NapProtoMsg, ProtoField } from '@napneko/nap-proto-core';
 import { MessageElement } from '@/core/packet/message/MessageElement';
+import { Attribute } from '@/core/packet/message/Attribute';
 
 export const RichText = new NapProtoMsg({
-    // attribute: ProtoField(1, () => Attribute.fields, true),
+    attribute: ProtoField(1, () => Attribute.fields, true),
     elements: ProtoField(2, () => MessageElement.fields, false, true),
     // notOnlineFile: ProtoField(3, () => NotOnlineFile.fields, true),
     // ptt: ProtoField(4, () => Ptt.fields, true),
