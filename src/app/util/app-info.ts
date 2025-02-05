f88c6a75c178efd8557970ee694afefc9e3b6112
Seijo Cecilia <willamwei@outlook.com>
2025-02-05 11:33:17 +0800

    [app] add Linux app info preset and fetch function

---
+++ src/app/util/app-info.ts
@@ -0,0 +1,25 @@
+import { AppInfo } from '@/core/common/AppInfo';
+
+export const LinuxAppInfoPreset: AppInfo = {
+    'Os': 'Linux',
+    'VendorOs': 'linux',
+    'Kernel': 'Linux',
+    'CurrentVersion': '3.2.15-30366',
+    'MiscBitmap': 32764,
+    'PtVersion': '2.0.0',
+    'SsoVersion': 19,
+    'PackageName': 'com.tencent.qq',
+    'WtLoginSdk': 'nt.wtlogin.0.0.1',
+    'AppId': 1600001615,
+    'SubAppId': 537258424,
+    'AppIdQrCode': 13697054,
+    'AppClientVersion': 30366,
+    'MainSigMap': 169742560,
+    'SubSigMap': 0,
+    'NTLoginType': 1,
+};
+
+export async function fetchAppInfoFromSignUrl(signUrl: string): Promise<AppInfo> {
+    return fetch(signUrl.endsWith('/') ? `${signUrl}appinfo` : `${signUrl}/appinfo`)
+        .then(res => res.json());
+}
