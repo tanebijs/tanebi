d6909d99066ebb8ed8dfc87faf4e4891ceac6d38
Seijo Cecilia <willamwei@outlook.com>
2025-02-06 15:17:34 +0800

    [core] add definition of SrcMsgElement.pbReserve

--- src/core/packet/message/element/SrcMsgElement.ts
+++ src/core/packet/message/element/SrcMsgElement.ts
@@ -9,7 +9,12 @@ export const SrcMsgElement = new NapProtoMsg({
     elems: ProtoField(5, () => MessageElement.fields, false, true),
     type: ProtoField(6, ScalarType.INT32, true, false),
     richMsg: ProtoField(7, ScalarType.BYTES, true, false),
-    pbReserve: ProtoField(8, ScalarType.BYTES, true, false),
+    pbReserve: ProtoField(8, () => ({
+        messageId: ProtoField(3, ScalarType.UINT64),
+        senderUid: ProtoField(6, ScalarType.STRING, true),
+        receiverUid: ProtoField(7, ScalarType.STRING, true),
+        friendSequence: ProtoField(8, ScalarType.UINT32, true),
+    }), true, false),
     sourceMsg: ProtoField(9, ScalarType.BYTES, true, false),
     toUin: ProtoField(10, ScalarType.UINT64, true, false),
     troopName: ProtoField(11, ScalarType.BYTES, true, false),
