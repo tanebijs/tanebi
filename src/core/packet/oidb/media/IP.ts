fa4dfa93563ba8ca6fdf089ef0aec28342edb399
Seijo Cecilia <willamwei@outlook.com>
2025-02-09 12:10:36 +0800

    [core] correct field names about IP

--- src/core/packet/oidb/media/IP.ts
+++ src/core/packet/oidb/media/IP.ts
@@ -3,7 +3,7 @@ import { NapProtoMsg, ProtoField, ScalarType } from '@napneko/nap-proto-core';
 export const IPv4 = new NapProtoMsg({
     outIP: ProtoField(1, ScalarType.UINT32, false, false),
     outPort: ProtoField(2, ScalarType.UINT32, false, false),
-    iniP: ProtoField(3, ScalarType.UINT32, false, false),
+    inIP: ProtoField(3, ScalarType.UINT32, false, false),
     inPort: ProtoField(4, ScalarType.UINT32, false, false),
     ipType: ProtoField(5, ScalarType.UINT32, false, false),
 });
@@ -11,7 +11,7 @@ export const IPv4 = new NapProtoMsg({
 export const IPv6 = new NapProtoMsg({
     outIP: ProtoField(1, ScalarType.BYTES, true, false),
     outPort: ProtoField(2, ScalarType.UINT32, false, false),
-    iniP: ProtoField(3, ScalarType.BYTES, true, false),
+    inIP: ProtoField(3, ScalarType.BYTES, true, false),
     inPort: ProtoField(4, ScalarType.UINT32, false, false),
     ipType: ProtoField(5, ScalarType.UINT32, false, false),
 });
--- src/core/packet/oidb/media/response/UploadResp.ts
+++ src/core/packet/oidb/media/response/UploadResp.ts
@@ -5,8 +5,8 @@ import { NapProtoMsg, ProtoField, ScalarType } from '@napneko/nap-proto-core';
 export const UploadResp = new NapProtoMsg({
     uKey: ProtoField(1, ScalarType.STRING, true, false),
     uKeyTtlSecond: ProtoField(2, ScalarType.UINT32, false, false),
-    iPv4s: ProtoField(3, () => IPv4.fields, false, true),
-    iPv6s: ProtoField(4, () => IPv6.fields, false, true),
+    ipv4s: ProtoField(3, () => IPv4.fields, false, true),
+    ipv6s: ProtoField(4, () => IPv6.fields, false, true),
     msgSeq: ProtoField(5, ScalarType.UINT64, false, false),
     msgInfo: ProtoField(6, () => MsgInfo.fields, true, false),
     ext: ProtoField(7, () => ({
@@ -19,7 +19,7 @@ export const UploadResp = new NapProtoMsg({
         subType: ProtoField(1, ScalarType.UINT32, false, false),
         uKey: ProtoField(2, ScalarType.STRING, true, false),
         uKeyTtlSecond: ProtoField(3, ScalarType.UINT32, false, false),
-        iPv4s: ProtoField(4, () => IPv4.fields, false, true),
-        iPv6s: ProtoField(5, () => IPv6.fields, false, true),
+        ipv4s: ProtoField(4, () => IPv4.fields, false, true),
+        ipv6s: ProtoField(5, () => IPv6.fields, false, true),
     }), false, true),
 });
