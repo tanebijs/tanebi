f7b29ea96f9051152b10d2bd26d83114d16a61e8
Seijo Cecilia <willamwei@outlook.com>
2025-02-09 12:06:37 +0800

    [core] consolidate RKeyInfo fields into DownloadRKeyResp

--- src/core/packet/oidb/media/response/DownloadRKeyResp.ts
+++ src/core/packet/oidb/media/response/DownloadRKeyResp.ts
@@ -1,6 +1,11 @@
-import { RKeyInfo } from './RKeyInfo';
-import { NapProtoMsg, ProtoField } from '@napneko/nap-proto-core';
+import { NapProtoMsg, ProtoField, ScalarType } from '@napneko/nap-proto-core';
 
 export const DownloadRKeyResp = new NapProtoMsg({
-    rKeys: ProtoField(1, () => RKeyInfo.fields, false, true),
+    rKeys: ProtoField(1, () => ({
+        rkey: ProtoField(1, ScalarType.STRING, true, false),
+        rkeyTtlSec: ProtoField(2, ScalarType.UINT64, false, false),
+        storeId: ProtoField(3, ScalarType.UINT32, false, false),
+        rkeyCreateTime: ProtoField(4, ScalarType.UINT32, true, false),
+        type: ProtoField(5, ScalarType.UINT32, true, false),
+    }), false, true),
 });
