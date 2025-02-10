3993ae063e1d27bd88f25492a20d91ded8964714
Wesley F. Young <willamwei@outlook.com>
2025-02-10 12:08:04 +0800

    [app] add default type param to BotContact

--- src/app/entity/BotContact.ts
+++ src/app/entity/BotContact.ts
@@ -2,7 +2,7 @@ import { Bot } from '@/app';
 import { BotEntity } from '@/app/entity';
 import { OutgoingSegment, OutgoingSegmentOf } from '@/core/message/outgoing';
 
-export abstract class BotContact<T extends { uin: number }> extends BotEntity<T> {
+export abstract class BotContact<T extends { uin: number } = { uin: number }> extends BotEntity<T> {
     protected constructor(
         public readonly bot: Bot,
         public readonly data: T,
