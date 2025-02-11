22176612fdd5e3d045c7b8946b8799b208034bcc
Wesley F. Young <willamwei@outlook.com>
2025-02-12 00:01:17 +0800

    [test] use stable api for message recv test

--- src/test/message.ts
+++ src/test/message.ts
@@ -1,14 +1,12 @@
 import bot from './fast';
 import { b } from '@/app/message';
 
-bot.ctx.events.on('messagePush', (msg) => {
-    if (msg) {
-        delete msg.internalElems;
-        console.log(msg);
-    }
-});
-
 const group = await bot.getGroup(0); // Substitute with your group's Uin
+
 console.log(await group?.sendMsg([
     b.text('Hello, this is a test message.'),
 ]));
+
+group?.onMessage((message, sender) => {
+    console.log(`${sender.card || sender.nickname} (${sender.uin}): [${message.type}] ${message.content.toPreviewString()}`);
+});
