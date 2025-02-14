4b516b21297ef5448fa0ebc2da7e2c11f4df5e49
Wesley F. Young <willamwei@outlook.com>
2025-02-14 14:00:18 +0800

    [test] change message recv logging style

--- src/test/message/receive.ts
+++ src/test/message/receive.ts
@@ -3,13 +3,13 @@ import bot from '../fast';
 
 bot.globalMsg.on('message', (msg) => {
     if (msg.contact instanceof BotFriend) {
-        console.log(`${msg.contact.nickname} (${msg.contact.uin}) >>>`);
-        console.log('    ' + msg.content.toPreviewString());
+        console.log(`${msg.contact.nickname} (${msg.contact.uin})`);
+        console.log('>>> ' + msg.content.toPreviewString());
     } else if (msg.contact instanceof BotGroup) {
         const group = msg.contact as BotGroup;
         msg.contact.getMember(msg.senderUin).then((member) => {
-            console.log(`${group.name} (${group.uin}) >>> ${member?.card || member?.nickname} (${msg.senderUin}) >>>`);
-            console.log('    ' + msg.content.toPreviewString());
+            console.log(`${group.name} (${group.uin}) ::: ${member?.card || member?.nickname} (${msg.senderUin})`);
+            console.log('>>> ' + msg.content.toPreviewString());
         });
     }
 });
