de2aeb2c04164049ad1369a527a85e4bbbdb7dc8
Wesley F. Young <willamwei@outlook.com>
2025-03-07 23:17:44 +0800

    [core] rearrange docs

--- packages/core/src/entity/request/BotFriendRequest.ts
+++ packages/core/src/entity/request/BotFriendRequest.ts
@@ -1,18 +1,7 @@
 export class BotFriendRequest {
     constructor(
-        /**
-         * The uin of who sent the friend request.
-         */
         readonly fromUin: number,
-
-        /**
-         * The uid of who sent the friend request.
-         */
         readonly fromUid: string,
-
-        /**
-         * The message with the friend request.
-         */
         readonly message: string,
 
         /**
