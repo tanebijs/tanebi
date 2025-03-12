db314c97372aaad74133f5c1f35aedbcddfa4ffc
Seijo Cecilia <willamwei@outlook.com>
2025-03-12 10:10:24 +0800

    [core] use symbol for internal field `raw`

--- packages/core/src/entity/BotFriendSendMsgRef.ts
+++ packages/core/src/entity/BotFriendSendMsgRef.ts
@@ -2,18 +2,25 @@ import { ctx } from '@/index';
 import { BotFriend } from '@/entity';
 import { OutgoingPrivateMessage } from '@/internal/message/outgoing';
 
+export const raw = Symbol('PrivateMessage raw');
+
 export class BotPrivateSendMsgRef {
+    readonly [raw]: OutgoingPrivateMessage;
+
     constructor(
         readonly sequence: number,
         readonly timestamp: number,
-        readonly raw: OutgoingPrivateMessage,
+        rawMsg: OutgoingPrivateMessage,
         readonly friend: BotFriend,
-    ) {}
+    ) {
+        this[raw] = rawMsg;
+    }
 
     /**
      * Recall this message
      */
     async recall() {
-        await this.friend.bot[ctx].ops.call('recallFriendMessage', this.friend.uid, this.raw.clientSequence, this.raw.random, this.timestamp, this.sequence);
+        await this.friend.bot[ctx].ops.call('recallFriendMessage',
+            this.friend.uid, this[raw].clientSequence, this[raw].random, this.timestamp, this.sequence);
     }
 }
