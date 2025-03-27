523bebd95f6eb165ea571df823e80cd348b268ef
Seijo Cecilia <willamwei@outlook.com>
2025-03-27 09:55:55 +0800

    [app] event base & message event

---
+++ packages/app/src/event/index.ts
@@ -0,0 +1,14 @@
+import { OneBotApp } from '@/index';
+
+export abstract class OneBotEvent {
+    readonly time: number;
+    readonly self_id: number;
+
+    constructor(
+        app: OneBotApp,
+        readonly post_type: 'message' | 'notice' | 'request' | 'meta_event',
+    ) {
+        this.time = Date.now();
+        this.self_id = app.bot.uin ?? 0;
+    }
+}
---
+++ packages/app/src/event/message.ts
@@ -0,0 +1,62 @@
+import { OneBotEvent } from '@/event';
+import { OneBotApp } from '@/index';
+
+export abstract class OneBotMessageEvent extends OneBotEvent {
+    constructor(
+        app: OneBotApp,
+        readonly message_type: 'private' | 'group',
+        readonly message_id: number,
+        readonly user_id: number,
+        readonly message: string, // TODO: Message type
+        readonly raw_message: string,
+        readonly font: number,
+    ) {
+        super(app, 'message');
+    }
+}
+
+export class OneBotPrivateMessageEvent extends OneBotMessageEvent {
+    constructor(
+        app: OneBotApp,
+        message_id: number,
+        user_id: number,
+        message: string,
+        raw_message: string,
+        font: number,
+        readonly sub_type: 'friend' | 'group' | 'other',
+        readonly sender: {
+            user_id: number;
+            nickname: string;
+            sex: string;
+            age: number;
+        },
+    ) {
+        super(app, 'private', message_id, user_id, message, raw_message, font);
+    }
+}
+
+export class OneBotGroupMessageEvent extends OneBotMessageEvent {
+    constructor(
+        app: OneBotApp,
+        message_id: number,
+        user_id: number,
+        message: string,
+        raw_message: string,
+        font: number,
+        readonly sub_type: 'normal' | 'notice',
+        readonly group_id: number,
+        readonly sender: {
+            user_id: number;
+            nickname: string;
+            card: string;
+            sex: string;
+            age: number;
+            area: string;
+            level: string;
+            role: string;
+            title: string;
+        }
+    ) {
+        super(app, 'group', message_id, user_id, message, raw_message, font);
+    }
+}
