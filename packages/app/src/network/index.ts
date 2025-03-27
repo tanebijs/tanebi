0afe9c1f4ab5b88b9da930fbee17557b4d880b79
Seijo Cecilia <willamwei@outlook.com>
2025-03-27 09:57:42 +0800

    [app] network base

---
+++ packages/app/src/network/index.ts
@@ -0,0 +1,12 @@
+import { OneBotEvent } from '@/event';
+import { OneBotApp } from '@/index';
+
+export abstract class OneBotNetworkAdapter<T> {
+    constructor(readonly app: OneBotApp, readonly adapterConfig: T) {}
+
+    abstract start(): void | Promise<void>;
+    
+    abstract stop(): void | Promise<void>;
+    
+    abstract emitEvent(event: OneBotEvent): void | Promise<void>;
+}
