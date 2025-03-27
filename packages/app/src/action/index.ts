8b003439dfc8963c39fa61fa98a0a81c6c05adc4
Seijo Cecilia <willamwei@outlook.com>
2025-03-27 09:55:37 +0800

    [app] refactor action structure

--- packages/app/src/action/index.ts
+++ packages/app/src/action/index.ts
@@ -1,6 +1,21 @@
-import { Failed, OneBotGeneralResponse } from '@/entity/response';
 import { OneBotApp } from '@/index';
 import { z } from 'zod';
+
+export interface OneBotGeneralResponse {
+    status: 'ok' | 'async' | 'failed';
+    retcode: number;
+    data: unknown;
+    msg?: string;
+    wording?: string;
+}
+
+export function Ok(data?: unknown): OneBotGeneralResponse {
+    return { status: 'ok', retcode: 0, data };
+}
+
+export function Failed(retcode: number, msg: string, wording: string = msg): OneBotGeneralResponse {
+    return { status: 'failed', retcode, data: null, msg, wording };
+}
 
 export interface OneBotAction {
     endpoint: string;
