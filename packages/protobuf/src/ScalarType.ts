3bc31df8c8c39a41e4d148c116382e9de7441fae
Seijo Cecilia <willamwei@outlook.com>
2025-04-09 14:32:10 +0800

    [protobuf] add scalar type

---
+++ packages/protobuf/src/ScalarType.ts
@@ -0,0 +1,17 @@
+export enum ScalarType {
+    DOUBLE = 1,
+    FLOAT = 2,
+    INT64 = 3,
+    UINT64 = 4,
+    INT32 = 5,
+    FIXED64 = 6,
+    FIXED32 = 7,
+    BOOL = 8,
+    STRING = 9,
+    BYTES = 12,
+    UINT32 = 13,
+    SFIXED32 = 15,
+    SFIXED64 = 16,
+    SINT32 = 17,
+    SINT64 = 18,
+}
