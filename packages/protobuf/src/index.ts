48ab3af8de95eb62bf5e482a59ac61232c797acc
Seijo Cecilia <willamwei@outlook.com>
2025-04-10 20:44:24 +0800

    [protobuf] export classes and types

---
+++ packages/protobuf/src/index.ts
@@ -0,0 +1,5 @@
+export { ProtoMessage } from './ProtoMessage';
+export { ProtoField } from './ProtoField';
+export { ScalarType } from './ScalarType';
+export type { ProtoModel, InferProtoModel, InferProtoModelInput } from './ProtoMessage';
+export type { ProtoSpec, InferProtoSpec, InferProtoSpecInput, ScalarTypeToTsType } from './ProtoField';
