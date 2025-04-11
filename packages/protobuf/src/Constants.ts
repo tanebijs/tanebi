ce77d75810701180672a94a281fa303289954f2f
Wesley F. Young <willamwei@outlook.com>
2025-04-11 13:29:48 +0800

    [all] use npm version for protobuf

--- packages/protobuf/src/Constants.ts
+++
@@ -1,4 +0,0 @@
-export const Fixed32Size = 4;
-export const Fixed64Size = 8;
-export const FloatSize = Fixed32Size;
-export const DoubleSize = Fixed64Size;
--- packages/protobuf/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { ProtoMessage } from './ProtoMessage';
-export { ProtoField } from './ProtoField';
-export { ScalarType } from './ScalarType';
-export type { ProtoModel, InferProtoModel, InferProtoModelInput } from './ProtoMessage';
-export type { ProtoSpec, InferProtoSpec, InferProtoSpecInput, ScalarTypeToTsType } from './ProtoField';
