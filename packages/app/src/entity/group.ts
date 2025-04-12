f7b3842608598461116d7e6a3486d6932c6b4208
Seijo Cecilia <willamwei@outlook.com>
2025-04-12 16:28:42 +0800

    [app] add entity type definition

---
+++ packages/app/src/entity/group.ts
@@ -0,0 +1,6 @@
+export interface OneBotGroup {
+    group_id: number;
+    group_name: string;
+    member_count: number;
+    max_member_count: number;
+}
---
+++ packages/app/src/entity/user.ts
@@ -0,0 +1,45 @@
+export interface OneBotFriend {
+    user_id: number;
+    qid?: string;
+    nickname: string;
+    remark: string;
+}
+
+export interface OneBotGroupMember {
+    group_id: number;
+    user_id: number;
+    nickname: string;
+    card: string;
+    sex: OneBotGender;
+    age: number;
+    join_time: number;
+    last_sent_time: number;
+    level: string;
+    role: OneBotGroupMemberRole;
+    unfriendly: boolean;
+    title: string;
+    title_expire_time: number;
+    card_changeable: boolean;
+}
+
+export interface OneBotStranger {
+    user_id: number;
+    qid?: string;
+    nickname: string;
+    sex: OneBotGender;
+    age: number;
+    level: number;
+    sign: string;
+}
+
+export enum OneBotGender {
+    Male = 'male',
+    Female = 'female',
+    Unknown = 'unknown',
+}
+
+export enum OneBotGroupMemberRole {
+    Owner = 'owner',
+    Admin = 'admin',
+    Member = 'member',
+}
