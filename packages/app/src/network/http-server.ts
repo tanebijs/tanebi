9ad7a15f2a5f86f34641af53af3bced049d7665e
Seijo Cecilia <willamwei@outlook.com>
2025-04-13 23:24:16 +0800

    [app] so goodbye express & ws

--- packages/app/src/network/http-server.ts
+++ packages/app/src/network/http-server.ts
@@ -1,85 +1,70 @@
-import express, { Express } from 'express';
-import http from 'node:http';
 import { Failed } from '@app/action';
 import { HttpServerAdapterConfig } from '@app/common/config';
 import { OneBotApp } from '@app/index';
 import { OneBotNetworkAdapter } from '@app/network';
+import { Hono } from 'hono';
+import { HttpBindings, serve } from '@hono/node-server';
+import { StatusCode } from 'hono/utils/http-status';
+import { Server } from 'node:http';
 
 export class OneBotHttpServerAdapter extends OneBotNetworkAdapter<HttpServerAdapterConfig> {
-    readonly expressApp: Express;
-    readonly httpServer: http.Server;
+    readonly honoApp;
+    httpServer: Server | undefined;
 
     constructor(app: OneBotApp, config: HttpServerAdapterConfig, id: string) {
         super(app, config, 'HttpServer', id);
-        this.expressApp = express();
-        this.expressApp.use((req, res, next) => {
-            if (req.method === 'POST') {
-                if (req.headers['content-type']?.startsWith('text/plain')) {
-                    // In case the client forgets to set the Content-Type header
-                    req.headers['content-type'] = 'application/json';
-                }
-    
-                if (req.headers['content-type'] !== 'application/json') {
-                    res.status(406).json(Failed(406, 'Unsupported Content-Type'));
-                    return;
-                }
-            }
-            next();
-        });
-        this.expressApp.use(express.json());
-
+        this.honoApp = new Hono<{ Bindings: HttpBindings }>();
         if (config.accessToken) {
-            this.expressApp.use((req, res, next) => {
+            this.honoApp.use(async (c, next) => {
                 let inputToken;
-                if (req.headers.authorization) {
-                    const [type, token] = req.headers.authorization.split(' ');
+                const authorization = c.req.header('Authorization');
+                if (authorization) {
+                    const [type, token] = authorization.split(' ');
                     if (type === 'Bearer') {
                         inputToken = token;
                     }
                 } else {
-                    inputToken = req.query['access_token'];
+                    inputToken = c.req.query('access_token');
                 }
 
                 if (!inputToken) {
-                    this.logger.warn(`${req.ip} -> ${req.path} (Unauthorized)`);
-                    res.status(401).json(Failed(401, 'Access token is missing'));
-                    return;
+                    this.logger.warn(`${c.env.incoming.socket.remoteAddress} -> ${c.req.path} (Unauthorized)`);
+                    c.status(401);
+                    return c.json(Failed(401, 'Access token is missing'));
                 }
 
                 if (inputToken !== config.accessToken) {
-                    this.logger.warn(`${req.ip} -> ${req.path} (Forbidden)`);
-                    res.status(403).json(Failed(403, 'Wrong access token'));
-                    return;
+                    this.logger.warn(`${c.env.incoming.socket.remoteAddress} -> ${c.req.path} (Forbidden)`);
+                    c.status(403);
+                    return c.json(Failed(403, 'Invalid access token'));
                 }
 
-                next();
+                await next();
             });
         }
 
-        this.expressApp.all('/:endpoint', async (req, res) => {
-            const endpoint = req.params.endpoint;
-            const payload = req.method === 'GET' ? req.query : req.body;
+        this.honoApp.all('/:endpoint', async (c) => {
+            const endpoint = c.req.param('endpoint');
+            const payload = c.req.method === 'GET' ? c.req.query() : await c.req.json();
             const start = Date.now();
             const response = await this.app.actions.handleAction(endpoint, payload);
             const end = Date.now();
-            this.logger.info(`${req.ip} -> ${req.path} (${response.retcode === 0 ? 'OK' : response.retcode} ${end - start}ms)`);
-            res.status(response.retcode >= 400 && response.retcode < 500 ? response.retcode : 200).json(response);
+            this.logger.info(`${c.env.incoming.socket.remoteAddress} -> ${c.req.path} (${response.retcode === 0 ? 'OK' : response.retcode} ${end - start}ms)`);
+            c.status((response.retcode >= 400 && response.retcode < 500 ? response.retcode : 200) as StatusCode);
+            return c.json(response);
         });
-
-        this.httpServer = http.createServer(this.expressApp);
     }
 
     override startImpl() {
-        return new Promise<void>((resolve) => {
-            this.httpServer.listen(this.adapterConfig.port, this.adapterConfig.host, () => {
-                this.logger.info(`Listening at ${this.adapterConfig.host}:${this.adapterConfig.port}`);
-                resolve();
-            });
-        });
+        this.httpServer = serve({
+            fetch: this.honoApp.fetch,
+            port: this.adapterConfig.port,
+            hostname: this.adapterConfig.host,
+        }) as Server;
     }
 
     override stopImpl() {
-        this.httpServer.closeAllConnections();
+        this.httpServer?.closeAllConnections();
     }
 
     // Http server does not emit events
